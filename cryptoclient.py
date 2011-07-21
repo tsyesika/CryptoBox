88dd10c8d3f61b6d1f636e655150c089289dae57
Philip Jackson <dr.eyebrows@gmail.com>
2011-07-21 13:59:05 +0100

    Some preliminary authentication code.

--- cryptoclient.py
+++ cryptoclient.py
@@ -2,14 +2,20 @@
 import socket, ssl, os, hashlib, struct
 from easyaes import encrypt, decrypt
 
+socket = socket.socket()
+print "Connecting..."
+socket.connect("85.211.57.20",4554)
+print "Connected"
 
-plaintext = open(r"C:\Users\Philip\python\cryptobox\testfile.txt","rb")
-iv = bytearray(hashlib.sha512("password").digest()[:16])
+email = raw_input("Enter email: ")
+password = raw_input("Enter password: ")
+passhash = hashlib.sha512(password).digest()
+#pad email with 00000000 bytes for transmission; making it 64 bytes long
+email += ord(0)*(64-len(email))
 
-outfile = open(r"C:\Users\Philip\python\cryptobox\cipher.txt","wb")
-outpath = r"C:\Users\Philip\python\cryptobox\cipher.txt"
-#outlist = []
-encrypt(plaintext,outfile,iv)
-ciphertext = open(r"C:\Users\Philip\python\cryptobox\cipher.txt","rb")
-decrypt(ciphertext,r"C:\Users\Philip\python\cryptobox\decrypted.txt",iv)
-ciphertext.close()
+socket.send(email+passhash)
+reply = socket.recv(1)
+if ord(reply) == 1:
+    print "Authentication successful"
+else:
+    print "Authentication failed"
