732740e8ec08df397cb9e2ed2bca4de74a6a3ec0
Philip Jackson <dr.eyebrows@gmail.com>
2011-07-21 15:14:18 +0100

    Fixed a mistake in cryptoclient. Fixed the listening socket in server. Also added some preliminary client authentication stuff to Connection_Handler.

--- cryptoclient.py
+++ cryptoclient.py
@@ -4,14 +4,14 @@ from easyaes import encrypt, decrypt
 
 socket = socket.socket()
 print "Connecting..."
-socket.connect("85.211.57.20",4554)
+socket.connect("85.211.57.20",7272)
 print "Connected"
 
 email = raw_input("Enter email: ")
 password = raw_input("Enter password: ")
 passhash = hashlib.sha512(password).digest()
 #pad email with 00000000 bytes for transmission; making it 64 bytes long
-email += ord(0)*(64-len(email))
+email += chr(0)*(64-len(email))
 
 socket.send(email+passhash)
 reply = socket.recv(1)
--- server.py
+++ server.py
@@ -8,7 +8,10 @@ import socket, hashlib, thread, server_config, ssl
 
 def Connection_Handler(sock):
 	""" Handles new connections to server """
-	pass
+	ID = sock.recv(128) #receive email and password hash from client
+	email = ID[:64]
+	email = email[email.index(chr(0))] #shave off the padding
+	passhash = ID[:64]
 	
 
 if __name__ == "__main__":
@@ -17,6 +20,7 @@ if __name__ == "__main__":
 		sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 	else:
 		sock = socket.socket()
+		sock.bind(config.bind_ip,config.bind_port)
 	while True:
 		sock, addr = sock.accept()
 		if server_config.sslwrap:
