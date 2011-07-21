f40934605e9341cc5e79634b013e6023a09184fa
Jessica T <xray7224@googlemail.com>
2011-07-21 05:19:06 +0100

    Adds a start… long way off anything...

---
+++ config.py
@@ -0,0 +1,18 @@
+##
+# Server Config
+# Version 0.1
+##
+
+# Bind IP
+bind_ip = ""
+
+# Bind Port
+bind_port = 7272
+
+# IPV6 Support for bind
+ipv6 = False
+
+# SSL Bind 
+sslwrap = True
+
+# -- Todo (add options of for certificates)
