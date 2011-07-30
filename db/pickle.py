0d14cc7735bdcf99119747eee0ef07658a34d30b
Jessica T <xray7224@googlemail.com>
2011-07-30 13:09:40 +0100

    Fixes inconsistant return type for init

--- db/pickle.py
+++ db/pickle.py
@@ -3,7 +3,7 @@ import shelve, os
 
 def init(x):
 	""" Starts a connection with database 'x' """
-	return shelve.open(x.database_name)
+	return [shelve.open(x.database_name), ""]
 
 def close(x):
 	""" Syncs and closes database connection """
@@ -13,7 +13,7 @@ def close(x):
 def login(x, email, password):
 	""" Checks if user with said email and password is correct first element of return arg will be true if valid """
 	# {"Users":{email:{UID:0, Password:"", Salt:""}, "Files":{uid:{filepath:[filepath, clonepath, hash, dateuploaded, filesize, encrypted, individual]}}}
-	if email in x.keys():
+	if email in x[0].keys():
 		if x["Users"][email]["Password"] == password:
 			return [True, ""]
 		else:
@@ -22,17 +22,17 @@ def login(x, email, password):
 
 def addfile(x, uid, filepath, clonepath, fhash, dateupload, filesize, encrypted=1, individual=0):
 	""" Adds a file to the db """
-	if uid in x["Files"].keys() and filepath in x["Files"][uid].keys():
+	if uid in x[0]["Files"].keys() and filepath in x[0]["Files"][uid].keys():
 		return [False, "File Already Exists."]
 	if not uid in x["Files"]:
-		x["Files"][uid] = {filepath:[clonepath, fhash, dateuploaded, filesize, encrypted, individual]}
+		x[0]["Files"][uid] = {filepath:[clonepath, fhash, dateuploaded, filesize, encrypted, individual]}
 	else:
-		x["Files"][uid][filepath] = [clonepath, fhash, dateuploaded, filesize, encrypted, individual]
+		x[0]["Files"][uid][filepath] = [clonepath, fhash, dateuploaded, filesize, encrypted, individual]
 	return [True, ""]
 
 def rmfile(x, uid, filepath, fhash):
 	""" Removes a file from db """
-	if uid in x["Files"].keys() and filepath in x["Files"][uid].keys():
+	if uid in x[0]["Files"].keys() and filepath in x[0]["Files"][uid].keys():
 		ntemp = x[uid]
 		del ntemp[filepath]
 		x[uid] = ntemp
@@ -41,11 +41,11 @@ def rmfile(x, uid, filepath, fhash):
 
 def calculateused(x, uid):
 	""" calculates total file size. """
-	return [sum([i for i in [x["Files"][uid][n][3] for n in x["Files"][uid].keys()]]), ""]
+	return [sum([i for i in [x[0]["Files"][uid][n][3] for n in x[0]["Files"][uid].keys()]]), ""]
 
 def getsalt(x, email):
 	""" Gets salt for user """
-	if email in x["Users"].keys():
+	if email in x[0]["Users"].keys():
 		return [x["Users"][email]["Salt"], ""]
 	return [False, "User does not exist."]
 
